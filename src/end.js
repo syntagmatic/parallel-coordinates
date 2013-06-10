b77e0a977d7c1624960ca6db839febec65c9c097
syntagmatic <kai.s.chang@gmail.com>
2013-06-09 23:19:50 -0700

    version bump

--- src/end.js
+++ src/end.js
@@ -1,6 +1,6 @@
   pc.toString = function() { return "Parallel Coordinates: " + __.dimensions.length + " dimensions (" + d3.keys(__.data[0]).length + " total) , " + __.data.length + " rows"; };
   
-  pc.version = "0.1.7";
+  pc.version = "0.2.0";
 
   return pc;
 };
