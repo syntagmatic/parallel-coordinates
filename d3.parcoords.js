b71388a4bbbb42e07041659d38a8e0d05aa9b65d
syntagmatic <kai.s.chang@gmail.com>
2014-05-20 21:21:36 -0700

    version bump, new examples and collaborators

--- d3.parcoords.js
+++ d3.parcoords.js
@@ -714,7 +714,7 @@ function position(d) {
 }
   pc.toString = function() { return "Parallel Coordinates: " + __.dimensions.length + " dimensions (" + d3.keys(__.data[0]).length + " total) , " + __.data.length + " rows"; };
   
-  pc.version = "0.2.2";
+  pc.version = "0.3.0";
 
   return pc;
 };
