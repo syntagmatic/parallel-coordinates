0c098cfa83333f6474cfa6995de5f57c89e0ab24
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-10-04 01:51:44 +0200

    Add a first (failing) test.
    This was somewhat painful to setup, but now all the required bits and
    pieces are there to start expanding our test suite.

---
+++ test/brushModes-test.js
@@ -0,0 +1,26 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    events = require('events'),
+    load = require('./load'),
+    suite = vows.describe('brushModes');
+
+function d3Parcoords() {
+  var promise = new(events.EventEmitter);
+  load(function(d3) {
+    promise.emit('success', d3.parcoords());
+  });
+  return promise;
+}
+
+suite.addBatch({
+  'd3.parcoords': {
+    'has by default': {
+       topic: d3Parcoords(),
+       'three brush modes': function(pc) {
+          assert.equal(pc.brushModes().length, 3);
+       }
+    },
+  }
+});
+
+suite.export(module);
---
+++ test/load.js
@@ -0,0 +1,43 @@
+var smash = require("smash"),
+    jsdom = require("jsdom");
+
+module.exports = (function() {
+  var files = ['node_modules/d3/d3.min.js', 'd3.parcoords.js'],
+      expression = 'd3',
+      sandbox = { console: console };
+
+  function document(html) {
+    var document = jsdom.jsdom(html);
+
+    sandbox = {
+      console: console,
+      document: document,
+      window: document.parentWindow,
+      setTimeout: setTimeout,
+      clearTimeout: clearTimeout,
+      Date: Date // so we can override Date.now in tests, and use deepEqual
+    };
+
+    return topic;
+  };
+
+  function topic(cb, html) {
+    var html = arguments.length >= 2
+      ? "<html><head></head><body></body></html>"
+      : html;
+
+    // d3 expects a global 'document' variable.
+    document(html);
+    smash.load(files, expression, sandbox, function(error, d3) {
+      if (error) console.trace(error.stack);
+      else cb(d3);
+    });
+  };
+
+  return topic;
+})();
+
+process.on("uncaughtException", function(e) {
+  console.trace(e.stack);
+});
+
