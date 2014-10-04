641b8122c3f160e2f5cc9a1b1c9825e890ccd6e5
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-10-04 20:43:28 +0200

    Add more tests that express the expected brush modes.

--- test/brushModes-test.js
+++ test/brushModes-test.js
@@ -17,7 +17,16 @@ suite.addBatch({
     'has by default': {
        topic: d3Parcoords(),
        'three brush modes': function(pc) {
-          assert.strictEqual(pc.brushModes().length, 3);
+         assert.strictEqual(pc.brushModes().length, 3);
+       },
+       'the brush mode "None"': function(pc) {
+         assert.notStrictEqual(pc.brushModes().indexOf("None"), -1);
+       },
+       'the brush mode "1D-Axis"': function(pc) {
+         assert.notStrictEqual(pc.brushModes().indexOf("1D-Axis"), -1);
+       },
+       'the brush mode "2D-strums"': function(pc) {
+         assert.notStrictEqual(pc.brushModes().indexOf("2D-strums"), -1);
        }
     },
   }
