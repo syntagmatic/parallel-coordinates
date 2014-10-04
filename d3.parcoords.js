26e22cf58fef50ed28ac810dd000ce86f525022b
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-10-04 21:15:07 +0200

    Add initial code for a brushMode function.

--- d3.parcoords.js
+++ d3.parcoords.js
@@ -636,6 +636,32 @@ pc.brushModes = function() {
   return Object.getOwnPropertyNames(brushModes);
 };
 
+pc.brushMode = function(mode) {
+  if (pc.brushModes().indexOf(mode) === -1) {
+    throw "pc.brushmode: Unsupported brush mode: " + mode;
+  }
+
+  if (arguments.length === 0) {
+    return __.brushMode;
+  }
+
+  // Make sure that we don't trigger unnecessary events by checking if the mode
+  // actually changes.
+  if (mode !== __.brushMode) {
+    // When changing brush modes, the first thing we need to do is clearing any
+    // brushes from the current mode, if any.
+    pc.brushReset();
+
+    // Next, we need to 'uninstall' the current brushMode.
+    brushModes[__.brushMode].uninstall(pc);
+    // Finally, we can install the requested one.
+    __.brushMode = mode;
+    brushModes[__.brushMode].install();
+  }
+
+  return pc;
+};
+
 // brush mode: 1D-Axes
 
 pc.brushable = function() {
