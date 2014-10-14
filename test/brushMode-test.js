55e54c7e7512a82b2b6d42b7660f2a2ecf775451
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-10-14 21:33:15 +0200

    First stab at extending test-suite for 1D-brush.
    It doesn't test much yet, but the goal is to properly test the DOM
    structure, after enabling and disabling 1D brushes.

--- test/brushMode-test.js
+++ test/brushMode-test.js
@@ -2,7 +2,16 @@ var vows = require('vows'),
     assert = require('assert'),
     events = require('events'),
     load = require('./load'),
-    suite = vows.describe('brushMode');
+    suite = vows.describe('brushMode'),
+    testData = [];
+
+testData = [
+  { x1: 1, x2: -1, x3: 1 },
+  { x1: 2, x2: -2, x3: 4 },
+  { x1: 3, x2: -3, x3: 9 },
+  { x1: 4, x2: -4, x3: 16 },
+  { x1: 5, x2: -5, x3: 25 },
+];
 
 function d3Parcoords() {
   var promise = new events.EventEmitter();
@@ -12,6 +21,23 @@ function d3Parcoords() {
     d3.select('body').append('div').attr('id', 'test');
     pc('div#test');
     promise.emit('success', pc);
+  });
+  return promise;
+}
+
+function d3ParcoordsWith1DBrush() {
+  var promise = new events.EventEmitter();
+
+  load(function(d3) {
+    var pc = d3.parcoords();
+
+    d3.select('body').append('div').attr('id', 'test');
+    pc('div#test')
+      .data(testData)
+      .render()
+      .brushMode('1D-axes');
+
+    promise.emit('success', d3, pc);
   });
   return promise;
 }
@@ -45,6 +71,18 @@ suite.addBatch({
         assert.strictEqual(pc.brushMode(), "2D-strums");
       }
     },
+  },
+  'When d3.parcoords has brushMode': {
+    '1D-axes': {
+      topic: d3ParcoordsWith1DBrush(),
+      'there should be three g.dimension elements': function(ev, d3, pc) {
+        var svg = d3.select('div#test').select('svg'),
+            dimensions = svg.select('g').selectAll('.dimension');
+
+        assert.strictEqual(dimensions.size(), 3);
+      }
+
+    }
   }
 });
 
